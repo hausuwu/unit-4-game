b50349021eca35c9e2e001ac884be52397b7b7e2
hausu <kennedymj2@gmail.com>
2019-06-03 21:51:25 -0500

    wrote puedo code for game

---
+++ javascript/game.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    // // assign random value to minerals required
+    // var compChoice = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+    // console.log(compChoice);
+    // $("#compguess").text(compChoice);
+
+});
+
+// varaiables
+// wins
+// loses
+// players score
+
+// functions
+
+ // assign random value to minerals required
+ var compChoice = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+ console.log(compChoice);
+ $("#compguess").text(compChoice);
+
+//  assign random value to minearal images
+
+// on click add mineral value to players score
+
+// if score meets minerals required reset all values and increment wins
+
+// else reset all values and increment loses
+
+
